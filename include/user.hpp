80ffdff068964d0757b77dd20542d6cf248d800a
EntireTwix <willkatz.dev@gmail.com>
2021-04-02 01:28:19 -0700

    added drogon

--- include/user.hpp
+++ include/user.hpp
@@ -8,6 +8,10 @@ private:
     uint_fast64_t balance;
     std::string password;
 
+    //for read/write of object's state concurrently
+    std::mutex bal_lock;
+    std::mutex pass_lock;
+
 public:
     /**
      * @brief User constructor
@@ -26,6 +30,7 @@ public:
 
     bool ChangePassword(const std::string &attempt, std::string &&new_pass)
     {
+        std::lock_guard<std::mutex> lock{pass_lock};
         const bool state = (password == attempt);
         if (state)
         {
@@ -45,7 +50,14 @@ public:
      */
     static bool SendFunds(User &a, User &b, uint_fast64_t amount, const std::string &attempt)
     {
-        const bool state = (a.password == attempt) && (a.balance >= amount);
+        bool state;
+        {
+            std::lock_guard<std::mutex> lock{a.pass_lock};
+            state = (a.password == attempt);
+        }
+
+        std::scoped_lock<std::mutex, std::mutex> lock{a.bal_lock, b.bal_lock};
+        state = state && (a.balance >= amount);
         if (state)
         {
             a.balance -= amount;
@@ -59,8 +71,9 @@ public:
      * 
      * @return the balance
      */
-    uint_fast64_t GetBal() const
+    uint_fast64_t GetBal()
     {
+        std::lock_guard<std::mutex> lock{bal_lock};
         return balance;
     }
 
@@ -70,8 +83,9 @@ public:
      * @param attempt the password
      * @return wether the passwords match
      */
-    bool VerifyPassword(const std::string &attempt) const
+    bool VerifyPassword(const std::string &attempt) //for connected services
     {
+        std::lock_guard<std::mutex> lock{pass_lock};
         return (password == attempt);
     }
 };
