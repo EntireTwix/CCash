1c7b2f67fe7e43e2fdba8097b7b8663c89e7a7af
EntireTwix <willkatz.dev@gmail.com>
2021-04-01 19:39:58 -0700

    :bug: ChangePassword accidentately didnt set new pass

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 
 int main()
 {
-    User x(1000, "pass123");
-    std::cout << x.ChangePassword("pass123", "newpass123");
+    User a(1000, "pass123");
+    User b(0, "pass123");
+    std::cout << a.ChangePassword("pass123", "newpass123") << '\n';
+    std::cout << SendFunds(a, b, 250, "newpass123") << '\n';
+    a.GetBal();
+    a.VerifyPassword("newpass124");
+    std::cout << a.GetBal() << '\n';
 
     return 0;
 }
