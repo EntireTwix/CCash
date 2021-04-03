048519c1a52a75ebc202394033d4ff5e1478aacb
EntireTwix <willkatz.dev@gmail.com>
2021-04-03 00:15:59 -0700

    :construction: working on main

--- main.cpp
+++ main.cpp
@@ -3,16 +3,28 @@
 #include <thread>
 #include "bank.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    std::thread([&]() {
-        for (int i = 0; i < 10000; ++i)
+    if (argc != 5)
+    {
+        std::cerr << "Usage: sudo ./bank <admin pass> <saving frequency in minutes> <run in background (1 or 0)> <threads>\n";
+        return 0;
+    }
+    if ((argv[3][0] != '0' && argv[3][0] != '1'))
+    {
+        std::cerr << "run in background state must be 1 or 0\n";
+        return 0;
+    }
+
+    //Auto Saving
+    volatile bool saving_flag = true;
+    std::thread([&argv, &saving_flag]() {
+        while (saving_flag)
         {
-            Bank.AddUser(std::to_string(i), "root");
+            std::this_thread::sleep_for(std::chrono::minutes(std::stoi(argv[2])));
+            Bank.Save();
         }
     }).detach();
-    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
-    Bank.Save();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+
     return 0;
 }
