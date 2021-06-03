ab1110161150fc90a5de632c9b651a63e88a805a
doggo <fearlessdoggo21@vivaldi.net>
2021-06-03 17:36:44 -0500

    Idiot changed the variable name

--- include/log_consts.hpp
+++ include/log_consts.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-// `max_log_size` must be divisible by `pre_alloc`
-constexpr auto max_log_size = 100;
-constexpr auto pre_alloc = 10;
+// `max_log_size` must be divisible by `pre_log_size`
+constexpr auto max_log_size = 100; // Setting to 0 does not compile logging
+constexpr auto pre_log_size = 10;
