5095ad0adbd70e09a1dfa6a4ab6bf2ae8a8873b1
William Katz <willkatz.dev@gmail.com>
2021-06-03 18:24:23 -0700

    legacy print

--- include/bank.hpp
+++ include/bank.hpp
@@ -182,7 +182,6 @@ public:
                     Json::Value temp;
                     for (uint32_t i = u.log.data.size(); i > 0; --i)
                     {
-                        std::cout << i << '\n';
                         temp[i - 1]["to"] = u.log.data[i - 1].to;
                         temp[i - 1]["from"] = u.log.data[i - 1].from;
                         temp[i - 1]["amount"] = (Json::UInt)u.log.data[i - 1].amount;
