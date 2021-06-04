2e05197791c4f18590da9bad80c010750dd23268
EntireTwix <willkatz.dev@gmail.com>
2021-06-03 18:39:18 -0700

    fixed build issues

--- include/log_consts.hpp
+++ include/log_consts.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 // `max_log_size` must be divisible by `pre_log_size`
-constexpr auto max_log_size = 100; // Setting to 0 does not compile logging
-constexpr auto pre_log_size = 10;
+constexpr unsigned max_log_size = 100; // Setting to 0 does not compile logging
+constexpr unsigned pre_log_size = 10;
