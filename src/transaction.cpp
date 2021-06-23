ac859f2c17aac19feee1d1de356cdb051bc7ab8a
EntireTwix <willkatz.dev@gmail.com>
2021-06-23 15:10:10 -0700

    :bug: forgot time_val in constructor

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,5 +1,5 @@
 #include "transaction.h"
 
 Transaction::Transaction() = default;
-Transaction::Transaction(std::string from_str, std::string to_str, uint32_t amount, time_t time) : from(from_str), to(to_str), amount(amount) {}
+Transaction::Transaction(std::string from_str, std::string to_str, uint32_t amount, time_t time_val) : from(from_str), to(to_str), amount(amount), time(time_val) {}
 Transaction::Transaction(std::string from_str, std::string to_str, uint32_t amount) : from(from_str), to(to_str), amount(amount) { time = std::time(NULL); }
