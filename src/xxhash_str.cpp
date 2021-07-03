fe98e733191a041053f0c1dab3e6ddb9d39fea29
EntireTwix <willkatz.dev@gmail.com>
2021-07-02 22:42:24 -0700

    :fire::art::racehorse: removed substr_view after learning string_view::substr is O(1)

--- src/xxhash_str.cpp
+++ src/xxhash_str.cpp
@@ -1,6 +1,10 @@
 #include "xxhash_str.h"
 
-XXH64_hash_t xxHashStringGen::operator()(const std::string_view &str) const noexcept
+XXH64_hash_t xxHashStringGen::operator()(const std::string &str) const noexcept
+{
+    return XXH3_64bits(str.data(), str.size());
+}
+XXH64_hash_t xxHashStringGen::operator()(std::string_view str) const noexcept
 {
     return XXH3_64bits(str.data(), str.size());
 }
