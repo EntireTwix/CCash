5ab0f89e89d41b843e5d9e27b385803558b21380
EntireTwix <willkatz.dev@gmail.com>
2021-07-06 20:29:27 -0700

    :racehorse::zap: noexcept and new constructor

--- src/bank_resp.cpp
+++ src/bank_resp.cpp
@@ -1,4 +1,5 @@
 #include "bank_resp.h"
 
-BankResponse::BankResponse() = default;
-BankResponse::BankResponse(drogon::HttpStatusCode code, std::string &&str) : first(code), second(str) {}
+BankResponse::BankResponse() noexcept  = default;
+BankResponse::BankResponse(drogon::HttpStatusCode code, std::string &&str) noexcept : first(code), second(str) {}
+BankResponse::BankResponse(drogon::HttpStatusCode code, const std::string &str) noexcept  : first(code), second(str) {}
