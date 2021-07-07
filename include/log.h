09911d5496e5cdccb32f4cc61ea197c014225b02
EntireTwix <willkatz.dev@gmail.com>
2021-07-06 20:28:37 -0700

    :construction::racehorse: preparing to use FBE, in the meantime its 99% faster

--- include/log.h
+++ include/log.h
@@ -10,16 +10,17 @@ struct Log
 {
 private:
     ChangeFlag log_flag;
-    Json::Value log_snapshot;
+    std::string log_snapshot;
 
 public:
+    Log() noexcept;
 #if MAX_LOG_SIZE == 1
     Transaction data;
 #else
     std::vector<Transaction> data;
 #endif
 
-    const Json::Value &GetLog() noexcept;
+    const std::string &GetLog() noexcept;
     void AddTrans(Transaction &&t) noexcept;
     Json::Value Serialize() const; // to be removed later
 };
