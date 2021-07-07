a0307f211d64214e0a0f11e5ea83c06547081c8d
EntireTwix <willkatz.dev@gmail.com>
2021-07-06 22:07:06 -0700

    :zap: updated benchmark

--- benchmarking.cpp
+++ benchmarking.cpp
@@ -69,30 +69,30 @@ int main(int argc, char **argv)
     bank.admin_account = "twix";
 
     const std::string data("this string is quite long which is relevant when testing the speed of a hasing function");
-    Op(std::hash<std::string>{}(data), "std::hash<std::string> ", 100000);
-    Op(xxHashStringGen{}(data), "xxHashStringGen ", 100000);
+    Op(std::hash<std::string>{}(data), "std::hash<std::string> ", 1000000);
+    Op(xxHashStringGen{}(data), "xxHashStringGen ", 1000000);
 
-    Op_a(bank.AddUser("", 0, ""), "add user: ", 100000, bank.DelUser(""));
-    Op(bank.ImpactBal("twix", 1), "impact bal: ", 100000);
-    Op(bank.SetBal("twix", 100000), "set bal: ", 100000);
-    Op(bank.SendFunds("twix", "jolly", 1), "send funds: ", 100000);
-    Op(bank.SendFunds("twix", "twix", 1), "invalid send funds: ", 100000);
+    Op_a(bank.AddUser("", 0, ""), "add user: ", 1000000, bank.DelUser(""));
+    Op(bank.ImpactBal("twix", 1), "impact bal: ", 1000000);
+    Op(bank.SetBal("twix", 1000000), "set bal: ", 1000000);
+    Op(bank.SendFunds("twix", "jolly", 1), "send funds: ", 1000000);
+    Op(bank.SendFunds("twix", "twix", 1), "invalid send funds: ", 1000000);
 
     bank.AddUser("", 0, "");
-    Op_a(bank.DelUser(""), "del user: ", 100000, bank.AddUser("", 0, ""));
+    Op_a(bank.DelUser(""), "del user: ", 1000000, bank.AddUser("", 0, ""));
     bank.DelUser("");
 
-    Op(bank.Contains("twix"), "contains: ", 100000);
-    Op(bank.AdminVerifyAccount("twix"), "admin verify pass: ", 100000);
-    Op(bank.GetBal("twix"), "get bal: ", 100000);
-    Op(bank.VerifyPassword("twix", "root"), "verify pass: ", 100000);
-    Op(bank.ChangePassword("twix", "root"), "change pass: ", 100000);
+    Op(bank.Contains("twix"), "contains: ", 1000000);
+    Op(bank.AdminVerifyAccount("twix"), "admin verify pass: ", 1000000);
+    Op(bank.GetBal("twix"), "get bal: ", 1000000);
+    Op(bank.VerifyPassword("twix", "root"), "verify pass: ", 1000000);
+    Op(bank.ChangePassword("twix", "root"), "change pass: ", 1000000);
 #if MAX_LOG_SIZE > 0
-    Op(bank.GetLogs("twix"), "get logs: ", 10000);
+    Op(bank.GetLogs("twix"), "get logs: ", 1000000);
 #endif
     Op(bank.Save(), "saving: ", 1);
 #if CONSERVATIVE_DISK_SAVE
-    Op(bank.GetChangeState(), "change flag: ", 100000);
+    Op(bank.GetChangeState(), "change flag: ", 1000000);
 #endif
 
     //GetBal scalining test
