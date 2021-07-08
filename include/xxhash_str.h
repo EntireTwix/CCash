175976e796c5a58c538ee2a34a4b83946ddc71a8
EntireTwix <willkatz.dev@gmail.com>
2021-07-08 14:58:30 -0700

    :zap: string move overload

--- include/xxhash_str.h
+++ include/xxhash_str.h
@@ -6,4 +6,5 @@ struct xxHashStringGen
 {
     XXH64_hash_t operator()(const std::string &str) const noexcept;
     XXH64_hash_t operator()(std::string_view &&str) const noexcept;
+    XXH64_hash_t operator()(std::string &&str) const noexcept;
 };
--- src/xxhash_str.cpp
+++ src/xxhash_str.cpp
@@ -8,3 +8,7 @@ XXH64_hash_t xxHashStringGen::operator()(std::string_view &&str) const noexcept
 {
     return XXH3_64bits(str.data(), str.size());
 }
+XXH64_hash_t xxHashStringGen::operator()(std::string &&str) const noexcept
+{
+    return XXH3_64bits(str.data(), str.size());
+}
