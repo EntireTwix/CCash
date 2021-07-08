805a7d05b36dccd528e274accc02de3d99b6f4a0
EntireTwix <willkatz.dev@gmail.com>
2021-07-08 02:59:27 -0700

    :racehorse: thread_local

--- src/bank_resp.cpp
+++ src/bank_resp.cpp
@@ -1,15 +1,15 @@
 #include "bank_resp.h"
 
-BankResponse::BankResponse() noexcept  = default;
+BankResponse::BankResponse() noexcept = default;
 BankResponse::BankResponse(drogon::HttpStatusCode code, std::string &&str) noexcept : first(code), second(str) {}
-BankResponse::BankResponse(drogon::HttpStatusCode code, const std::string &str) noexcept  : first(code), second(str) {}
+BankResponse::BankResponse(drogon::HttpStatusCode code, const std::string &str) noexcept : first(code), second(str) {}
 
 template <>
 drogon::HttpResponsePtr drogon::toResponse(BankResponse &&data)
 {
-    auto res = std::make_shared<HttpResponseImpl>(data.first, CT_APPLICATION_JSON);
+    static thread_local auto res = std::make_shared<HttpResponseImpl>(data.first, CT_APPLICATION_JSON);
     res->setBody(std::move(data.second));
-    auto &advices = HttpAppFrameworkImpl::instance().getResponseCreationAdvices();
+    static thread_local auto &advices = HttpAppFrameworkImpl::instance().getResponseCreationAdvices();
     if (!advices.empty())
     {
         for (auto &advice : advices)
