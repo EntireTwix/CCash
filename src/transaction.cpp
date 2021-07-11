36e2f6301fd4d68803236b36083794300c3114f6
EntireTwix <willkatz.dev@gmail.com>
2021-07-10 20:01:05 -0700

    :racehorse: noexcept constructors

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,5 +1,5 @@
 #include "transaction.h"
 
-Transaction::Transaction() = default;
-Transaction::Transaction(const std::string &from_str, const std::string &to_str, uint32_t amount, time_t time_val) : from(from_str), to(to_str), amount(amount), time(time_val) {}
-Transaction::Transaction(const std::string &from_str, const std::string &to_str, uint32_t amount) : from(from_str), to(to_str), amount(amount) { time = std::time(NULL); }
+Transaction::Transaction() noexcept = default;
+Transaction::Transaction(const std::string &from_str, const std::string &to_str, uint32_t amount, time_t time_val) noexcept : from(from_str), to(to_str), amount(amount), time(time_val) {}
+Transaction::Transaction(const std::string &from_str, const std::string &to_str, uint32_t amount) noexcept : from(from_str), to(to_str), amount(amount) { time = std::time(NULL); }
