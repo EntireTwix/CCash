37b110d36f3bc33fc78952020c7ed733aa871065
EntireTwix <willkatz.dev@gmail.com>
2022-12-17 12:25:47 -0800

    :bug: receiving field should not be in quotes

--- src/log.cpp
+++ src/log.cpp
@@ -68,9 +68,9 @@ std::string Log::GetLogsV2() noexcept
         {
             log_snapshot_v2 += "{\"counterparty\":\"";                                             //17
             log_snapshot_v2 += data[i].counterparty;                                               //max_name_size?
-            log_snapshot_v2 += "\",\"receiving\":\"";                                              //15
+            log_snapshot_v2 += "\",\"receiving\":";                                              //15
             log_snapshot_v2 += std::to_string(data[i].receiving);                                  //4
-            log_snapshot_v2 += "\",\"amount\":";                                                   //11
+            log_snapshot_v2 += ",\"amount\":";                                                   //11
             log_snapshot_v2 += std::to_string(data[i].amount);                                     //10?
             log_snapshot_v2 += ",\"time\":";                                                       //8
             log_snapshot_v2 += std::to_string(data[i].time);                                       //10?
