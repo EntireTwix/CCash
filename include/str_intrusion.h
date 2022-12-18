2c62c89b4784c8e596de61b140704875cfedb6c9
EntireTwix <willkatz.dev@gmail.com>
2022-12-18 13:22:46 -0800

    :bulb: clarifying comment

--- include/str_intrusion.h
+++ include/str_intrusion.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <string>
 
+/*
+The intended use of this cursed class is in violating the encapsulation of std::string this class acts like std::string_view even though its stored in a std::string.
+The reason this was needed is because sometimes we have a std::string_view instance and another library requires a const std::string& argument, forcing us to copy to a string before passing it, this copying is unnecessary.
+*/
+
 struct StrFromSV_Wrapper
 {
     std::string str;
